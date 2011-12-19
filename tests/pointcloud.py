49033a808bc29fc93689d7d19b435a13c289dd9e
Pablo Márquez Neila <pmarquezneila@gmail.com>
2011-12-19 06:27:46 +0100

    Point cloud reconstruction with RGB data for color
    pointcloud.py works with fixed calibration values in
    kinect_calib.py.

--- tests/pointcloud.py
+++ tests/pointcloud.py
@@ -30,10 +30,14 @@ gl_rgb_texture = None
 gl_depth_texture = None
 
 # Prepare the points.
-points = np.reshape(np.mgrid[:640,:480], (2,-1))
-texcoords = points / np.array([[640.0], [480.0]])
-points = np.insert(points, 2, 1, 0)
-points = np.dot(la.inv(kc.K_rgb), points)*100
+vertices = np.reshape(np.mgrid[:480,:640][[1,0]], (2,-1))
+texcoords = vertices / np.array([[640.0], [480.0]])
+vertices = np.insert(vertices, 2, 1, 0)
+vertices = np.dot(la.inv(kc.K_ir), vertices)
+
+#depth_map = np.array([123.6 * np.tan(i/2842.5 + 1.1863) for i in xrange(2048)])
+depth_map = np.array([1000.0 / (i * -0.0030711016 + 3.3309495161) for i in xrange(2048)])
+depth_map[2047] = 0.0
 
 def init_gl(width, height):
     global gl_rgb_texture
@@ -52,30 +56,52 @@ def resize_event(width, height):
     gl.glViewport(0, 0, width, height)
     gl.glMatrixMode(gl.GL_PROJECTION)
     gl.glLoadIdentity()
-    # glu.gluOrtho2D(0, width, 0, height)
     glu.gluPerspective(60, width/float(height), 50.0, 8000.0)
     gl.glMatrixMode(gl.GL_MODELVIEW)
 
 def display():
     gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
     
+    #rgb_img = freenect.sync_get_video(format=freenect.VIDEO_IR_8BIT)[0]
     rgb_img = freenect.sync_get_video()[0]
+    depth_img = freenect.sync_get_depth()[0]
+    depth = depth_map[depth_img]
     
+    current_vertices = vertices * depth.ravel()
+    
+    # Extract texcoords.
+    aux = np.insert(current_vertices, 3, 1, 0)
+    aux = np.dot(kc.K_rgb, np.dot(kc.T, aux)[:3,:])
+    aux = aux / aux[2,:]
+    texcoords = aux[:2,:] / np.array([[640.0], [480.0]])
+    
+    gl.glEnable(gl.GL_TEXTURE_2D)
     gl.glBindTexture(gl.GL_TEXTURE_2D, gl_rgb_texture)
     gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, 3, 640, 480, 0,
                     gl.GL_RGB, gl.GL_UNSIGNED_BYTE, rgb_img)
     
     gl.glLoadIdentity()
     glu.gluLookAt(*camera.get_glulookat_parameters())
-    gl.glBegin(gl.GL_QUADS)
-    gl.glTexCoord2d(0.0, 1.0)
-    gl.glVertex3d(-10, -10, -10)
-    gl.glTexCoord2d(0.0, 0.0)
-    gl.glVertex3d(-10, 10, -10)
-    gl.glTexCoord2d(1.0, 0.0)
-    gl.glVertex3d(10, 10, -10)
-    gl.glTexCoord2d(1.0, 1.0)
-    gl.glVertex3d(10, -10, -10)
+    gl.glEnableClientState(gl.GL_VERTEX_ARRAY)
+    gl.glEnableClientState(gl.GL_TEXTURE_COORD_ARRAY)
+    gl.glVertexPointer(3, gl.GL_DOUBLE, 0, current_vertices.T.ravel())
+    gl.glTexCoordPointer(2, gl.GL_DOUBLE, 0, texcoords.T.ravel())
+    gl.glPointSize(1)
+    gl.glColor3d(1,1,1)
+    gl.glDrawArrays(gl.GL_POINTS, 0, 640*480)
+    
+    # Draw axes indicator.
+    gl.glDisableClientState(gl.GL_VERTEX_ARRAY)
+    gl.glDisableClientState(gl.GL_TEXTURE_COORD_ARRAY)
+    gl.glDisable(gl.GL_TEXTURE_2D)
+    gl.glPointSize(5)
+    gl.glBegin(gl.GL_POINTS)
+    gl.glColor3d(1, 0, 0)
+    gl.glVertex3d(100.0, 0, 0)
+    gl.glColor3d(0, 1, 0)
+    gl.glVertex3d(0, 100.0, 0)
+    gl.glColor3d(0, 0, 1)
+    gl.glVertex3d(0, 0, 100.0)
     gl.glEnd()
     
     glut.glutSwapBuffers()
