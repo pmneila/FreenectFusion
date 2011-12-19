076f36cde011693318e94c9f14af4c1c25ba1299
Pablo Márquez Neila <pmarquezneila@gmail.com>
2011-12-19 03:30:27 +0100

    Created an orbit camera

---
+++ tests/orbitcamera.py
@@ -0,0 +1,58 @@
+
+import math
+from math import cos, sin
+from operator import add, mul
+
+def cross(v1, v2):
+    return (v1[1]*v2[2] - v1[2]*v2[1],
+            v1[2]*v2[0] - v1[0]*v2[2],
+            v1[0]*v2[1] - v1[1]*v2[0])
+
+def normalize(v):
+    length = math.sqrt(v[0]**2 + v[1]**2 + v[2]**2)
+    return tuple(map(lambda x: x/length, v))
+
+class OrbitCamera(object):
+    
+    def __init__(self):
+        self.center = (0.0,)*3
+        self.radius = 1000.0
+        self.longitude = 3*math.pi/2.0
+        self.latitude = 0.0
+    
+    def rotate(self, dlongitude, dlatitude):
+        self.longitude += dlongitude
+        self.latitude += dlatitude
+        if self.latitude >= math.pi/2.0:
+            self.latitude = math.pi/2.0 - 1e-3
+        if self.latitude <= -math.pi/2.0:
+            self.latitude = -math.pi/2.0 + 1e-3
+    
+    def translate(self, dx, dy):
+        # Get the perpendicular plane.
+        v = self.get_vector()
+        e1 = normalize(cross(v, (0,1,0)))
+        e2 = normalize(cross(v, e1))
+        e1 = map(lambda x: x*dx, e1)
+        e2 = map(lambda x: x*dy, e2)
+        displ = map(add, e1, e2)
+        self.center = tuple(map(add, self.center, displ))
+    
+    def zoom(self, dy):
+        step = self.radius/100
+        if dy > 100:
+            dy = 100
+        self.radius += step*dy
+    
+    def get_position(self):
+        return tuple(map(add, self.center, self.get_vector()))
+    
+    def get_vector(self):
+        return (cos(self.longitude)*cos(self.latitude)*self.radius,
+                sin(self.latitude)*self.radius,
+                sin(self.longitude)*cos(self.latitude)*self.radius)
+    
+    def get_glulookat_parameters(self):
+        return self.position + self.center + (0,1,0)
+    
+    position = property(get_position)
--- tests/pointcloud.py
+++ tests/pointcloud.py
@@ -1,40 +1,134 @@
 
 import sys
+from operator import sub
+
+import numpy as np
+import numpy.linalg as la
 import OpenGL.GL as gl
 import OpenGL.GLU as glu
 import OpenGL.GLUT as glut
 import freenect
 
+from orbitcamera import OrbitCamera
+import kinect_calib as kc
+
+class CameraState:
+    NONE = 0
+    ROTATION = 1
+    TRANSLATION = 2
+    ZOOM = 3
+
 window = None
 
+key_state = [False]*256
+mouse_position = None
+
+camera = OrbitCamera()
+camera_state = CameraState.NONE
+
+gl_rgb_texture = None
+gl_depth_texture = None
+
+# Prepare the points.
+points = np.reshape(np.mgrid[:640,:480], (2,-1))
+texcoords = points / np.array([[640.0], [480.0]])
+points = np.insert(points, 2, 1, 0)
+points = np.dot(la.inv(kc.K_rgb), points)*100
+
 def init_gl(width, height):
+    global gl_rgb_texture
+    
     gl.glClearColor(0.2, 0.2, 0.2, 0.0)
     gl.glEnable(gl.GL_DEPTH_TEST)
+    gl.glEnable(gl.GL_TEXTURE_2D)
+    gl_rgb_texture = gl.glGenTextures(1)
+    gl.glBindTexture(gl.GL_TEXTURE_2D, gl_rgb_texture)
+    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR)
+    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
+    
     resize_event(width, height)
 
 def resize_event(width, height):
     gl.glViewport(0, 0, width, height)
     gl.glMatrixMode(gl.GL_PROJECTION)
+    gl.glLoadIdentity()
+    # glu.gluOrtho2D(0, width, 0, height)
+    glu.gluPerspective(60, width/float(height), 50.0, 8000.0)
+    gl.glMatrixMode(gl.GL_MODELVIEW)
 
 def display():
     gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+    
+    rgb_img = freenect.sync_get_video()[0]
+    
+    gl.glBindTexture(gl.GL_TEXTURE_2D, gl_rgb_texture)
+    gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, 3, 640, 480, 0,
+                    gl.GL_RGB, gl.GL_UNSIGNED_BYTE, rgb_img)
+    
+    gl.glLoadIdentity()
+    glu.gluLookAt(*camera.get_glulookat_parameters())
+    gl.glBegin(gl.GL_QUADS)
+    gl.glTexCoord2d(0.0, 1.0)
+    gl.glVertex3d(-10, -10, -10)
+    gl.glTexCoord2d(0.0, 0.0)
+    gl.glVertex3d(-10, 10, -10)
+    gl.glTexCoord2d(1.0, 0.0)
+    gl.glVertex3d(10, 10, -10)
+    gl.glTexCoord2d(1.0, 1.0)
+    gl.glVertex3d(10, -10, -10)
+    gl.glEnd()
+    
     glut.glutSwapBuffers()
 
-def keyboard_event(key, x, y):
+def keyboard_press_event(key, x, y):
     global window
+    global key_state
     
     if key == chr(27):
+        freenect.sync_set_led(1)
         glut.glutDestroyWindow(window)
         sys.exit(0)
+    
+    key_state[ord(key)] = True
+
+def keyboard_up_event(key, x, y):
+    global key_state
+    key_state[ord(key)] = False
 
 def mouse_button_event(button, state, x, y):
-    pass
+    
+    global mouse_position, camera_state
+    
+    mouse_position = (x,y)
+    
+    if state == glut.GLUT_UP:
+        camera_state = CameraState.NONE
+    elif button == glut.GLUT_LEFT_BUTTON:
+        camera_state = CameraState.ROTATION
+    elif button == glut.GLUT_RIGHT_BUTTON:
+        camera_state = CameraState.ZOOM
+    elif button == glut.GLUT_MIDDLE_BUTTON:
+        camera_state = CameraState.TRANSLATION
 
 def mouse_moved_event(x, y):
-    pass
+    
+    global mouse_position, camera, camera_state
+    
+    offset = map(sub, (x,y), mouse_position)
+    
+    if camera_state == CameraState.ROTATION:
+        camera.rotate(*[i/100.0 for i in offset])
+    elif camera_state == CameraState.TRANSLATION:
+        camera.translate(offset[0], -offset[1])
+    elif camera_state == CameraState.ZOOM:
+        camera.zoom(offset[1])
+    
+    mouse_position = (x,y)
 
 def main():
     global window
+    
+    freenect.sync_set_led(2)
     
     glut.glutInit()
     glut.glutInitDisplayMode(glut.GLUT_RGBA | glut.GLUT_DOUBLE
@@ -46,7 +140,8 @@ def main():
     glut.glutDisplayFunc(display)
     glut.glutIdleFunc(display)
     glut.glutReshapeFunc(resize_event)
-    glut.glutKeyboardFunc(keyboard_event)
+    glut.glutKeyboardFunc(keyboard_press_event)
+    glut.glutKeyboardUpFunc(keyboard_up_event)
     glut.glutMouseFunc(mouse_button_event)
     glut.glutMotionFunc(mouse_moved_event)
     
